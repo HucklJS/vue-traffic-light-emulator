c5b449a24f5333aca1f714a8ba3a9cd4e6507e0d
Vasiliy <vasilij.1357@yandex.ru>
2020-11-16 16:45:56 +0300

    Add wepback, loaders and plugins to project. Make basic file structure

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+<!--    <HelloWorld msg="Hello VAsya"/>-->
+    <h1>Hello again</h1>
+  </div>
+</template>
+
+<script>
+  // import HelloWorld from "./components/HelloWorld"
+  export default {
+    // components: {
+    //   HelloWorld
+    // }
+  }
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+h1 {
+  color: red;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main.js',
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ],
+
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      title: "Webpack-Vue App",
+    }),
+    new VueLoaderPlugin()
+  ],
+}
